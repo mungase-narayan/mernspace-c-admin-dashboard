2febbc4ba1cae474d7154d26db5e79e4498e010b
mungase-narayan <narayanmungase03@gmail.com>
2024-11-20 19:52:03 +0530

    tailwind css setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Hellow, Narayan Mungase</h1>
+      <h1 className="flex items-center justify-center text-3xl font-mono pt-4">
+        Hellow, Narayan Mungase
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
