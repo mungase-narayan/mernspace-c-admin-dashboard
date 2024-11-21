4451a4bf6a2af1b37d6817bd90606bb1af47b31f
mungase-narayan <narayanmungase03@gmail.com>
2024-11-21 18:54:16 +0530

    add: home and dashboard page

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <div className="flex items-center justify-center font-mono pt-4 text-red-500">
+      DashboardPage
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="flex items-center justify-center font-mono pt-4 text-red-500">
+      HomePage
+    </div>
+  );
+};
+
+export default HomePage;
