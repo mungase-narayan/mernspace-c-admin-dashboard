845e7cf955955ca1af687ae9e929418da40d2f13
mungase-narayan <narayanmungase03@gmail.com>
2024-11-27 01:58:27 +0530

    add: dashboard page

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,19 +1,88 @@
 import { useAuthStore } from "../store";
-import React from "react";
-import { Link, Navigate, Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { self } from "../http/api";
+import { useQuery } from "@tanstack/react-query";
+import { ImSpinner6 } from "react-icons/im";
+import { AxiosError } from "axios";
+
+import { AppSidebar } from "../components/AppSidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "../components/ui/breadcrumb";
+import { Separator } from "../components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "../components/ui/sidebar";
+
+const getSelf = async () => {
+  const { data } = await self();
+  return data;
+};
 const Dashboard = () => {
-  const { user } = useAuthStore();
+  const { setUser, user } = useAuthStore();
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["self"],
+    queryFn: getSelf,
+    retry: (failureCount: number, error) => {
+      if (error instanceof AxiosError && error.response?.status == 401) {
+        return false;
+      }
+      return failureCount < 3;
+    },
+  });
+
+  useEffect(() => {
+    if (data) {
+      setUser(data);
+    }
+  }, [data, setUser]);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center pt-10">
+        <div className="text-black animate-spin">
+          <ImSpinner6 />
+        </div>
+      </div>
+    );
+  }
+
   if (user === null) {
     return <Navigate to="/auth/login" replace={true} />;
   }
   return (
-    <div>
-      <h1 className="flex flex-col items-center justify-center font-mono pt-4 text-red-500">
-        Dashboard Layout
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
         <Outlet />
-        <Link to={"/auth/login"}>Go to login page</Link>
-      </h1>
-    </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 };
 
