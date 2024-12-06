dea73292de59f35c87dbb6ac0decf8e1c284a00c
mungase-narayan <narayanmungase03@gmail.com>
2024-12-06 22:16:52 +0530

    add: apis

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,8 +1,16 @@
-import { Credentials } from "@/types";
+import { CreateUserData, Credentials, User } from "../types";
 import { api } from "./client";
 
+export const register = (userData: User) => api.post("users", userData);
 export const login = (credentials: Credentials) =>
   api.post("auth/login", credentials);
 
 export const self = () => api.get("auth/self");
 export const logout = () => api.post("auth/logout");
+export const getUsers = (queryString: string) =>
+  api.get(`/users/getAll?${queryString}`);
+export const createUser = (user: CreateUserData) => api.post(`/users`, user);
+export const updateUser = (user: CreateUserData, id: string) =>
+  api.patch(`/users/${id}`, user);
+export const getTenants = (queryString: string) =>
+  api.get(`/tenants/getList?${queryString}`);
