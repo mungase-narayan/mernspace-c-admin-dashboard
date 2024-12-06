d0a4df0eb26370e5716a7532e80fa97db5ce9fce
mungase-narayan <narayanmungase03@gmail.com>
2024-12-06 22:13:23 +0530

    add: types

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,32 @@ export type Credentials = {
   email: string;
   password: string;
 };
+
+export type User = {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  createdAt: string;
+  tenant: Tenant | null;
+};
+
+export type CreateUserData = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  role: string;
+  tenantId: number;
+};
+
+export type Tenant = {
+  id: number;
+  name: string;
+  address: string;
+};
+
+export type FieldData = {
+  name: string[];
+  value?: string;
+};
