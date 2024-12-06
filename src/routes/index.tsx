97335745bfc97ca9fa77dc9f4552dd50355a1491
mungase-narayan <narayanmungase03@gmail.com>
2024-12-06 22:19:38 +0530

    add: routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,6 +10,12 @@ import LoginPage from "../pages/auth/LoginPage";
 import NonAuth from "../layouts/NonAuth";
 import Dashboard from "../layouts/Dashboard";
 import MainHomePage from "../pages/MainHomePage";
+import MenuPage from "../pages/dashboard/MenuPage";
+import OrdersPage from "../pages/dashboard/OrdersPage";
+import SalesPage from "../pages/dashboard/SalesPage";
+import PromosPage from "../pages/dashboard/PromosPage";
+import UsersPage from "../pages/dashboard/users/UsersPage";
+import RestaurantsPage from "../pages/dashboard/RestaurantsPage";
 
 const Routers = createBrowserRouter(
   createRoutesFromElements(
@@ -18,6 +24,12 @@ const Routers = createBrowserRouter(
 
       <Route path="dashboard" element={<Dashboard />}>
         <Route path="home" element={<DashboardHomePage />} />
+        <Route path="menu" element={<MenuPage />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="sales" element={<SalesPage />} />
+        <Route path="promos" element={<PromosPage />} />
+        <Route path="users" element={<UsersPage />} />
+        <Route path="restaurants" element={<RestaurantsPage />} />
       </Route>
 
       <Route path="auth" element={<NonAuth />}>
